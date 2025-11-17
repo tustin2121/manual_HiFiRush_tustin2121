bf167abac3e385a0eccc0dd7d23e78614a4421af
tustin2121 <tustin2121@gmail.com>
2025-11-17 17:58:28 -0500

    build script

---
+++ build.js
@@ -0,0 +1,131 @@
+#! node
+
+const { Transform, pipeline } = require('node:stream');
+const fs = require('fs/promises');
+const fsSync = require("fs");
+const PATH = require('path');
+const yaml = require('js-yaml');
+const archiver = require('archiver');
+
+// Thanks to https://github.com/ad-si/yaml2json/blob/master/source/index.js
+class YamlToJsonTransform extends Transform {
+	/** @type {Buffer} */ #buffer;
+	/**
+	 * 
+	 * @param {import('node:stream').TransformOptions} opts 
+	 */
+	constructor(opts={}) {
+		super(opts);
+		this.#buffer = new Buffer.alloc(0);
+	}
+	
+	_transform(data, encoding, done) {
+		this.#buffer = Buffer.concat([this.#buffer, data]);
+		done();
+	}
+	_flush(done) {
+		let json = yaml.load(this.#buffer.toString('utf8'));
+		if (this.readableObjectMode) {
+			this.push(json);
+		} else {
+			this.push(JSON.stringify(json, undefined, 4));
+		}
+		done();
+	}
+}
+
+
+async function main() {
+	let prefix = 'manual_undefined_undefined';
+	{
+		const str = await fs.readFile(`src/game.yml`, { encoding:'utf8' });
+		const data = yaml.load(str);
+		prefix = `manual_${data['game']}_${data['creator']}`;
+	}
+	await fs.mkdir('out', { recursive:true });
+	
+	const out = fsSync.createWriteStream(PATH.resolve(__dirname, 'out', `${prefix}.apworld`));
+	const zip = archiver('zip', { zlib: { level: 9 } });
+	
+	zip.on('close', ()=>{
+		console.log(`APWorld archive (${zip.pointer()} bytes) written to ${out.path}`);
+	});
+	zip.on('warning', (err)=> console.error('Warning archiving data:', err));
+	zip.pipe(out);
+	
+	for (const f of await fs.readdir('src')) {
+		if (!f.endsWith('yml')) continue;
+		console.log('Converting', f);
+		const rs = fsSync.createReadStream(`src/${f}`);
+		const ts = new YamlToJsonTransform();
+		rs.pipe(ts);
+		zip.append(ts, { name: `${PATH.basename(f, 'yml')}.json`, prefix:`${prefix}/data` });
+	}
+	for (const f of await fs.readdir('dist', { recursive:true, withFileTypes:false })) {
+		if (f.startsWith('data')) continue;
+		if (!PATH.extname(f)) continue;
+		console.log('Outputting', f);
+		const rs = fsSync.createReadStream(PATH.join('dist', f));
+		zip.append(rs, { name: f, prefix });
+	}
+	// for (const f of await fs.readdir('dist', { recursive:true, withFileTypes:true })) {
+	// 	if (f.parentPath.endsWith('data')) continue;
+	// 	if (f.isDirectory()) continue;
+	// 	console.log('Outputting', f.parentPath, f.name);
+	// 	const rs = fsSync.createReadStream(PATH.join(f.parentPath, f.name));
+	// 	zip.append(rs, { name: f.name, prefix });
+	// }
+	zip.finalize();
+}
+main();
+
+/*
+const fs = require('fs/promises');
+const fsSync = require("fs");
+const PATH = require('path');
+const yaml = require('js-yaml');
+
+async function emitJson(filename) {
+	const inFile = PATH.join(__dirname, 'src', PATH.basename(filename, 'yml'));
+	const outFile = PATH.join(__dirname, 'out', 'data', PATH.basename(filename, 'yml')) + '.json';
+	
+	const str = await fs.readFile(inFile, { encoding:'utf8' });
+	const data = yaml.load(str);
+	const json = JSON.stringify(data);
+	return fs.writeFile(outFile, json);
+}
+
+function makeArchive() {
+	return new Promise((res, rej)=>{
+		const out = fsSync.createWriteStream(PATH.resolve(__dirname, 'out', PATH.basename(outDir)+'.apworld'));
+		const zip = archiver('zip', {
+			zlib: { level: 9 },
+		});
+		zip.on('warning', (err)=> console.error('Warning archiving data:', err));
+		zip.on('close', ()=>{
+			console.log(`EPUB archive (${zip.pointer()} bytes) written to ${out.path}`);
+		});
+		zip.pipe(out);
+		zip.directory(outDir, false);
+		zip.finalize(); // This is a promise, but it fires before the file handle closes, 
+		// thus causing a bad zip file to be created that can't be opened by readers
+		out.on('close', res);
+	})
+}
+
+async function main() {
+	{
+		
+		await fs.mkdir('out/manual_hifirush_tustin2121');
+	}
+	
+	
+	
+	await Promise.all(
+		(await fs.readdir(PATH.join(__dirname, 'src'))).map(emitJson)
+	);
+	
+	
+}
+main();
+*/
